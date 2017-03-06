a0de252aa1a731590582c59a271bab5da2d294b8
Billy <wwpierce020602@gmail.com>
2017-03-06 13:43:52 -0800

    Renamed files

--- auto-stabilization/MPU_6050.ino
+++ auto-stabilization/MPU_6050.ino
@@ -1,6 +1,5 @@
 /*
    MODIFIED FROM MPU6050_DMP6 example code by "Jeff Rowberg".
-
 */
 #include "I2Cdev.h"
 #include "MPU6050_6Axis_MotionApps20.h"
--- auto-stabilization/PID.ino
+++ auto-stabilization/PID.ino
@@ -1,6 +1,6 @@
 /*
- * Based on Joop Brooking's tutorial/code: http://www.brokking.net/ymfc-al_main.html.
- */
+   Based on Joop Brooking's tutorial/code: http://www.brokking.net/ymfc-al_main.html.
+*/
 
 float pitch_pid_p = 0.0;
 float pitch_pid_i = 0.0;
@@ -12,7 +12,7 @@ float pitch_pid_i_mem;
 float pitch_pid_last_error;
 float pitch_pid_output;
 
-float calculatePitchPID(float pitch){
+float calculatePitchPID(float pitch) {
   pitch_pid_error_temp = pitch - pitch_pid_setpoint;
   pitch_pid_i_mem += pitch_pid_i * pitch_pid_error_temp;
   pitch_pid_output = pitch_pid_p * pitch_pid_error_temp + pitch_pid_i_mem + pitch_pid_d * (pitch_pid_error_temp - pitch_pid_last_error);

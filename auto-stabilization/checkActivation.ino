7ca96f6fb0146bb66485f8effeb09485d1fef781
Billy <wwpierce020602@gmail.com>
2017-03-06 18:30:32 -0800

    Added function to make sure everything was activated

---
+++ auto-stabilization/checkActivation.ino
@@ -0,0 +1,31 @@
+int disableCounter;
+int enableCounter;
+boolean activated;
+
+void readyActivation(){
+  activated = false;
+}
+
+void checkForActivation(){
+  if (getThrottle() < 1170 && getRudder() > 1895 && activated) {
+    enableCounter = 0;
+    disableCounter += 1;
+    if (disableCounter > 50) {
+      activated = false;
+      Serial.println("DEACTIVATED");
+    }
+  }
+  if (getThrottle() < 1170 && getRudder() < 1142 && !activated) {
+    disableCounter = 0;
+    enableCounter += 1;
+    if (enableCounter > 50) {
+      activated = true;
+      Serial.println("ACTIVATED");
+    }
+  }
+}
+
+boolean getActivation(){
+  return activated;
+}
+

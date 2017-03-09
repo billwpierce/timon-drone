5cd984680549f6f962ef9d16b620ec2bc30d7489
Billy <wwpierce020602@gmail.com>
2017-03-08 20:14:26 -0800

    added safeValue to make sure that if zeroValue is too high, we can still stop the drone; removed unnecesary /2 when adding pitch pid, because the output can be changed by a factor of two just by changing the PID constants

--- auto-stabilization/motorControl.ino
+++ auto-stabilization/motorControl.ino
@@ -5,6 +5,7 @@
 Servo motor1;
 Servo motor2;
 int zeroValue = 55;
+int safeValue = 50;
 
 void initializeMotors(){
   motor1.attach(11);
@@ -14,14 +15,14 @@ void initializeMotors(){
 }
 
 void runMotors(int throttle, float pitch){
-  float motor1Value = throttle - pitch / 2;
-  float motor2Value = throttle + pitch / 2;
-  motor1.write(map(motor1Value, 1151, 1908, 55, 180));
-  motor2.write(map(motor2Value, 1151, 1908, 55, 180));
+  float motor1Value = throttle - pitch;
+  float motor2Value = throttle + pitch;
+  motor1.write(map(motor1Value, 1151, 1908, zeroValue, 180));
+  motor2.write(map(motor2Value, 1151, 1908, zeroValue, 180));
 }
 
 void stopMotors(){
-  motor1.write(zeroValue);
-  motor2.write(zeroValue);
+  motor1.write(safeValue);
+  motor2.write(safeValue);
 }
 
